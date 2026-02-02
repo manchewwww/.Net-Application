c41fae9b48cb7c2a1cc796744c8a6829380c19ea
Georgi Manchev <georgi.m@first.bet>
2026-02-02 15:12:57 +0200

    feat: implement ICurrencyService and CurrencyService with CRUD operations

---
+++ AutoPartsShop/Services/Currency.cs
@@ -0,0 +1,64 @@
+using AutoPartsShop.Converters;
+using AutoPartsShop.Dtos;
+using AutoPartsShop.Repositories;
+using AutoPartsShop.Exceptions;
+
+namespace AutoPartsShop.Services
+{
+    public interface ICurrencyService
+    {
+        public Task<CurrencyResponse> AddCurrencyAsync(CurrencyCreateRequest request);
+        public Task<IEnumerable<CurrencyResponse>> GetAllCurrenciesAsync();
+        public Task<CurrencyResponse?> GetCurrencyByIdAsync(long id);
+        public Task<CurrencyResponse> UpdateCurrencyAsync(long id, CurrencyUpdateRequest request);
+        public Task<CurrencyResponse> DeleteCurrencyAsync(long id);
+    }
+
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly ICurrencyRepository _currencyRepository;
+
+        public CurrencyService(ICurrencyRepository currencyRepository)
+        {
+            _currencyRepository = currencyRepository;
+        }
+
+        public async Task<CurrencyResponse> AddCurrencyAsync(CurrencyCreateRequest request)
+        {
+            var currencyEntity = request.ToEntity();
+            var createdCurrency = await _currencyRepository.AddCurrencyAsync(currencyEntity);
+            return createdCurrency.ToDto();
+        }
+
+        public async Task<IEnumerable<CurrencyResponse>> GetAllCurrenciesAsync()
+        {
+            var currencies = await _currencyRepository.GetAllCurrencyAsync();
+            return currencies.Select(c => c.ToDto());
+        }
+
+        public async Task<CurrencyResponse?> GetCurrencyByIdAsync(long id)
+        {
+            var currency = await _currencyRepository.GetCurrencyByIdAsync(id) ?? throw new NotFoundException($"Currency with ID {id} not found.");
+            return currency?.ToDto();
+        }
+
+        public async Task<CurrencyResponse> UpdateCurrencyAsync(long id, CurrencyUpdateRequest request)
+        {
+            var existing = await _currencyRepository.GetCurrencyByIdAsync(id) ?? throw new NotFoundException($"Currency with ID {id} not found.");
+            existing.CurrencyCode = request.CurrencyCode;
+            existing.Name = request.Name;
+            existing.Symbol = request.Symbol;
+
+            await _currencyRepository.UpdateCurrencyAsync(existing);
+            return existing.ToDto();
+        }
+
+        public async Task<CurrencyResponse> DeleteCurrencyAsync(long id)
+        {
+            var currency = await _currencyRepository.GetCurrencyByIdAsync(id) ?? throw new NotFoundException($"Currency with ID {id} not found.");
+            await _currencyRepository.DeleteCurrencyAsync(id);
+            return currency.ToDto();
+        }
+    }
+
+}
