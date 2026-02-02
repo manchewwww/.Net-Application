80c6c268ba29d897d1bb0110c0d3570ca8a111fb
Georgi Manchev <georgi.m@first.bet>
2026-02-02 11:31:19 +0200

    Add BrandConverter for mapping between BrandEntity and DTOs

---
+++ AutoPartsShop/Converters/Brand.cs
@@ -0,0 +1,36 @@
+using AutoPartsShop.Entities;
+using AutoPartsShop.Dtos;
+
+namespace AutoPartsShop.Converters
+{
+    public static class BrandConverter
+    {
+        public static BrandResponse ToDto(this BrandEntity entity)
+        {
+            return new BrandResponse(
+                Id: (int)entity.Id,
+                Name: entity.Name,
+                Country: entity.CountryCode ?? string.Empty
+            );
+        }
+
+        public static BrandEntity ToEntity(this BrandCreateRequest dto)
+        {
+            return new BrandEntity
+            {
+                Name = dto.Name,
+                CountryCode = dto.Country
+            };
+        }
+
+        public static BrandEntity ToEntity(this BrandUpdateRequest dto)
+        {
+            return new BrandEntity
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                CountryCode = dto.Country
+            };
+        }
+    }
+}
