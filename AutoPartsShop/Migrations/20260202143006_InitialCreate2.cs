82b2eb06d5e2252787fd98ab4aad71710176fd02
Georgi Manchev <georgi.m@first.bet>
2026-02-02 16:32:05 +0200

    Refactor currency and country code columns in migrations
    - Renamed 'currency_code' to 'code' in the 'currencies' table and updated the corresponding index.
    - Renamed 'country_code' to 'code' in the 'countries' table.
    - Updated the model snapshot to reflect these changes.
    - Adjusted logging settings in appsettings.json to reduce verbosity for Entity Framework Core.

---
+++ AutoPartsShop/Migrations/20260202143006_InitialCreate2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoPartsShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "currency_code",
+                table: "currencies",
+                newName: "code");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_currencies_currency_code",
+                table: "currencies",
+                newName: "IX_currencies_code");
+
+            migrationBuilder.RenameColumn(
+                name: "country_code",
+                table: "countries",
+                newName: "code");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "code",
+                table: "currencies",
+                newName: "currency_code");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_currencies_code",
+                table: "currencies",
+                newName: "IX_currencies_currency_code");
+
+            migrationBuilder.RenameColumn(
+                name: "code",
+                table: "countries",
+                newName: "country_code");
+        }
+    }
+}
