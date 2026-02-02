dc5311dc3dc47d6dfd4020e2fc66138137c1f66e
Georgi Manchev <georgi.m@first.bet>
2026-02-02 15:29:55 +0200

    Add unique index on currency_code in currencies table
    - Created a new migration to add a unique index on the currency_code column in the currencies table to ensure data integrity.
    - Updated the model snapshot to reflect the new index.

---
+++ AutoPartsShop/Migrations/20260202132839_InitialCreate1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoPartsShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_currencies_currency_code",
+                table: "currencies",
+                column: "currency_code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_currencies_currency_code",
+                table: "currencies");
+        }
+    }
+}
