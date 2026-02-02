6b38a3a099046dccbe7f6988c1a4b0c1d70abf0b
Georgi Manchev <georgi.m@first.bet>
2026-02-02 10:14:28 +0200

    Add IBrandService interface and Brand class implementation for brand management

---
+++ AutoPartsShop/Services/Brand.cs
@@ -0,0 +1,49 @@
+using AutoPartsShop.Repositories;
+
+namespace AutoPartsShop.Services
+{
+
+    public interface IBrandService
+    {
+        public Task<Entities.BrandEntity> AddBrandAsync(Entities.BrandEntity brand);
+        public Task<IEnumerable<Entities.BrandEntity>> GetAllBrandsAsync();
+        public Task<Entities.BrandEntity?> GetBrandByIdAsync(int id);
+        public Task UpdateBrandAsync(Entities.BrandEntity brand);
+        public Task DeleteBrandAsync(Entities.BrandEntity brand);
+    }
+
+    public class Brand
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public Brand(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public async Task<Entities.BrandEntity> AddBrandAsync(Entities.BrandEntity brand)
+        {
+            return await _brandRepository.AddBrandAsync(brand);
+        }
+
+        public async Task<IEnumerable<Entities.BrandEntity>> GetAllBrandsAsync()
+        {
+            return await _brandRepository.GetAllBrandsAsync();
+        }
+
+        public async Task<Entities.BrandEntity?> GetBrandByIdAsync(int id)
+        {
+            return await _brandRepository.GetBrandByIdAsync(id);
+        }
+
+        public async Task UpdateBrandAsync(Entities.BrandEntity brand)
+        {
+            await _brandRepository.UpdateBrandAsync(brand);
+        }
+
+        public async Task DeleteBrandAsync(Entities.BrandEntity brand)
+        {
+            await _brandRepository.DeleteBrandAsync(brand);
+        }
+    }
+}
